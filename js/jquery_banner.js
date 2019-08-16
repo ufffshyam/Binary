103427a9d98add712ce99fd5f45cd0a2210ebe04
ufffshyam <44051021+ufffshyam@users.noreply.github.com>
2019-08-16 17:42:39 +0530

    Update jquery_banner.js

--- js/jquery_banner.js
+++ js/jquery_banner.js
@@ -9,7 +9,7 @@
         if (isVisited) {
             $(this).css('display', 'none');
             return false;
-        };
+        }
         this.append('<div id="video-play-check-prompt" class="modal-prompt"><img src="'+options.url+'" height="100%" width="100%"/><button class="button-enter btn styled-submit" data-params='+JSON.stringify(options)+'>X</button></div>');
         $(this).find('.modal-prompt').css({'background': '#ffffff', 'border': '12px solid #f2f2f2cc', 'box-shadow': '0px 0px 10px rgba(0,0,0,0.5)','width': (options.width ? options.width : '590px'),'height': (options.height ? options.height : '400px'),'margin': '0 auto','position': 'relative','top': '17%','z-index': '1000000','max-width': '1189px','max-height':'400px'});
         $(this).find('.modal-prompt .styled-submit').css({'display':'inline-block','position':'ABSOLUTE','top':'-22px','right':'-22px','background':'black','color':'#fff','font-size':'20px','padding':'6px 10px','border':'0px','border-radius':'50%','cursor':'pointer'});
